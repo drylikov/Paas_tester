213592346a7120a631f3ee3ded708f606de64810
drylikov <denis.rylikov@protonmail.com>
2025-06-03 00:00:47 +0300

    Paas_tester
    A little hello world for testing paases like heroku and no.de. Ignore this.

---
+++ server.js
@@ -0,0 +1,23 @@
+var express = "not found"
+var fs = require("fs")
+try {
+  express = JSON.parse(fs.readFileSync(require.resolve("express/package.json")).toString())
+} catch (ex) {
+  express = "can't load package.json data"
+}
+
+console.error("express is: "+express)
+
+var util = require("util")
+
+require("http").createServer(function (req, res) {
+  var data = { env: process.env
+             , module: module
+             , express: express
+             , pid: process.pid
+             , argv: process.argv
+             }
+  data = new Buffer(util.inspect(data))
+  res.writeHead(200, { "content-type":"text/plain", "content-length": data.length })
+  res.end(data)
+}).listen(+process.env.PORT || 3000)
